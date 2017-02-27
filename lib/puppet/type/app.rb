e760621fe21e87760f55110f4204a05f6a621f1e
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-02-27 15:04:42 +0100

    Remove existence check for passwordfile

--- lib/puppet/type/app.rb
+++ lib/puppet/type/app.rb
@@ -67,12 +67,6 @@ Puppet::Type.newtype(:app) do
 
   newparam(:passwordfile) do
     desc "The file containing the password for the user."
-
-    validate do |value|
-      unless File.exists? value
-        raise ArgumentError, "%s does not exists" % value
-      end
-    end
   end
 
   newparam(:user) do
