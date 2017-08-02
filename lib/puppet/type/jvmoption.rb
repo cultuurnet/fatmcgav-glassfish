ac04c28c2b2c8fe742cfa2c98270b115e0d5b18a
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-08-02 16:54:38 +0200

    Remove file existence check from jvmoption type

--- lib/puppet/type/jvmoption.rb
+++ lib/puppet/type/jvmoption.rb
@@ -56,12 +56,6 @@ Puppet::Type.newtype(:jvmoption) do
 
   newparam(:passwordfile) do
     desc "The file containing the password for the user."
-
-    validate do |value|
-      unless File.exists? value
-        raise ArgumentError, "%s does not exists" % value
-      end
-    end
   end
 
   newparam(:user) do
